07b63987ea4febcfd90eeabf8101f480f46577cb
Victor Aluko <victor.aluko@gmail.com>
2017-04-08 16:44:25 +0300

    Updated gitignore and package.json

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
-var http = require('http')
+var https = require('https');
+var http = require('http');
 var server = http.createServer();
 
-server.on('request', function(request, response){
-  response.writeHead(200, {'Content-Type': 'application/json'});
-  var query = require('url').parse(request.url, true).path;
-  http.get('http://api.fixer.io' + query, function(res){
-    var buffer = "";
-    res.on('data', function(chunk) {
-      buffer += chunk;
-    });
+var baseUri = 'https://graph.ir.ee';
+var predictPath = '/predictions/organizations/ee-11735006/bankruptcy-risk-scores';
+var apiKey = 'c2t5cGUyMDE3XzI6anhacTVDS2Q=';
+var options = {
+    host: baseUri,
+    //port: 8000,
+    path: predictPath,
+    headers: {
+        'Authorization': 'Basic ' + apiKey
+    }
+};
 
-    res.on('end', function(){
-      response.write(buffer);
-      response.end();
+server.on('request', function(request, response){
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    //var query = require('url').parse(request.url, true).path;
+    console.log(options);
+    https.get(options, function(res){
+        var buffer = "";
+        res.on('data', function(chunk) {
+            buffer += chunk;
+        });
+
+        res.on('end', function(){
+            response.write(buffer);
+            response.end();
+        });
     });
-  });
 });
 var port = process.env.PORT || 8080;
 server.listen(port);
 console.log("Listening on port " + port + "......")
 
-module.exports = server
+module.exports = server;
