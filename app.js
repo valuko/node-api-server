58fdae0253929c73aed43dca019e420cd2533ba0
Victor Aluko <victor.aluko@gmail.com>
2017-04-08 17:34:33 +0300

    Fixed the issue with app.json

--- app.js
+++ app.js
@@ -2,22 +2,22 @@ var https = require('https');
 var http = require('http');
 var server = http.createServer();
 
-var baseUri = 'https://graph.ir.ee';
+var baseUri = 'graph.ir.ee';
 var predictPath = '/predictions/organizations/ee-11735006/bankruptcy-risk-scores';
 var apiKey = 'c2t5cGUyMDE3XzI6anhacTVDS2Q=';
 var options = {
+    protocol: 'https:',
     host: baseUri,
-    //port: 8000,
+    port: 443,
     path: predictPath,
     headers: {
-        'Authorization': 'Basic ' + apiKey
+        'Authorization': 'Basic ' + apiKey,
+        'Content-Type': 'application/json'
     }
 };
 
 server.on('request', function(request, response){
     response.writeHead(200, {'Content-Type': 'application/json'});
-    //var query = require('url').parse(request.url, true).path;
-    console.log(options);
     https.get(options, function(res){
         var buffer = "";
         res.on('data', function(chunk) {
